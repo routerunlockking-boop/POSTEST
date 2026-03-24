2568945436ac643cf89b632f610c8c6e2874f4bc
Pamidu <routerunlockking@gmail.com>
2026-03-24 17:22:19 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     }
 
     try {
-        const uri = process.env.MONGO_URI || 'mongodb+srv://test:Abc%4012345@cluster0.a77j2uu.mongodb.net/?appName=Cluster0';
+        const uri = process.env.MONGO_URI || 'mongodb+srv://ravimotors:10669Abc@cluster0.mqdicae.mongodb.net/testdb?retryWrites=true&w=majority';
         const db = await mongoose.connect(uri, {
             serverSelectionTimeoutMS: 5000 // Tweak timeout down so Serverless fails faster instead of hanging
         });
@@ -74,8 +74,8 @@ const initializeDatabase = async () => {
         const adminExists = await User.findOne({ role: 'admin' });
         if (!adminExists) {
             await User.create({
-                email: 'smartzonelk101@gmail.com',
-                password: '200723800385@',
+                email: 'Admin',
+                password: 'Admin',
                 business_name: 'Admin Portal',
                 role: 'admin'
             });
